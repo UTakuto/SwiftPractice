52e86ecf6e6ef33f699bbae4f836aab7ab41a179
UTakuto <taku.0307.ue@icloud.com>
2024-09-10 01:30:03 +0900

    LaztVGrid 作成

---
+++ SampleLazyVGrid/SampleLazyVGrid/SampleLazyVGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleLazyVGridApp.swift
+//  SampleLazyVGrid
+//
+//  Created by 上森拓翔 on 2024/09/10.
+//
+
+import SwiftUI
+
+@main
+struct SampleLazyVGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
