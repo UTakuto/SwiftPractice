0b1db4372cf8cde2010429ab6c32f2487917d463
UTakuto <taku.0307.ue@icloud.com>
2024-09-10 01:12:16 +0900

    ToDoList 完成

--- ToDoList/ToDoList/ContentView.swift
+++ ToDoList/ToDoList/ContentView.swift
@@ -7,15 +7,42 @@
 
 import SwiftUI
 
+struct TaskData: Identifiable{
+    var title: String
+    var completed: Bool
+    var id = UUID()
+}
+
 struct ContentView: View {
+//    let taskData = ["ジョギングをする" , "散歩をする" , "一年生2人目決める" , "不正をなくす" , "不正を暴く"]
+    @State var taskData = [
+        TaskData(title: "ジョギングする" , completed: false),
+        TaskData(title: "散歩をする" , completed: false),
+        TaskData(title: "一年生2人目決める" , completed: false),
+        TaskData(title: "不正をなくす" , completed: false),
+        TaskData(title: "不正を暴く" , completed: false)
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            List(0..<taskData.count , id:\.self){index in
+                Button{
+                    taskData[index].completed.toggle()
+                }label:{
+                    HStack{
+                        Image(systemName: taskData[index].completed ? "checkmark.circle.fill" : "circle")
+//                        if taskData[index].completed{
+//                            Image(systemName: "checkmark.circle.fill")
+//                        }
+//                        else{
+//                            Image(systemName: "circle")
+//                        }
+                        Text(taskData[index].title)
+                    }.foregroundColor(.primary)
+                }
+            }
+            .navigationTitle("ToDoList")
         }
-        .padding()
     }
 }
 
